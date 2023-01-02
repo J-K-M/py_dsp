d113cc03bc46a15c01d56a9c177253afded97e15
J-K-M <90958043+J-K-M@users.noreply.github.com>
2023-01-02 21:04:15 +0000

    fix grid for odd numbers of bits per symbol

--- qam.py
+++ qam.py
@@ -5,7 +5,7 @@ from itertools import product
 time = np.arange(0, 100 * np.pi)
 
 # Number of bits per QAM symbol
-n = 4
+n = 6
 mode = 'grid'
 
 def split_bits(bits: list[int]) -> tuple[list[int]]:
@@ -13,7 +13,7 @@ def split_bits(bits: list[int]) -> tuple[list[int]]:
     return bits[len(bits)//2:], bits[:len(bits)//2]
 
 def map_iq(i_bits: list[int], q_bits: list[int]) -> tuple[float]:
-    ''' take in-phase and quadrature bits and generate I-Q symbol '''
+    ''' takes in-phase and quadrature bits to generate an IQ symbol '''
     i = 0
     for bit in i_bits:
         i = (i << 1) | bit
@@ -29,48 +29,54 @@ def map_iq(i_bits: list[int], q_bits: list[int]) -> tuple[float]:
 
 # loop over all possible bit combinations i.e. (0,0),(0,1),(1,0),(1,1)
 # and generate lists of IQ symbols and IQ waveforms
-i_symbols, q_symbols = [], []
-i_waves, q_waves = [], []
-for a in product((0, 1), repeat=n):
-    I, Q = map_iq(*split_bits(a))
-    i_symbols.append(I)
-    q_symbols.append(Q)
-    
-    i_waves.append(I * np.cos(time/10))
-    q_waves.append(Q * np.sin(time/10))
+iq_symbols = []
+for bits in product((0, 1), repeat=n):
+    i, q = map_iq(*split_bits(bits))
+    iq_symbols.append((i, q))
 
+# ensure that symbols appear in the correct order in grid
+iq_symbols.sort()
+
+iq_waves = []
+for iq in iq_symbols:
+    # iq_waves.append(np.exp(2*np.pi*iq*time/10))
+    iq_waves.append((
+            iq[0] * np.cos(time/10),
+            iq[1] * np.sin(time/10)
+    ))
 
 if mode != 'grid':
     # set up IQ and Sum plot axis for side-by-side plotting
-    num_plot_rows = 2**n
-    ax_iq = [plt.subplot2grid((num_plot_rows, 2), (0, 0))]
-    ax_sum = [plt.subplot2grid((num_plot_rows, 2), (0, 1))]
-    for x in range(1, num_plot_rows):
-        ax_iq.append(plt.subplot2grid((num_plot_rows, 2), (x, 0)))
-        ax_sum.append(plt.subplot2grid((num_plot_rows, 2), (x, 1)))
+    num_rows = 2**n
+    ax_iq = [plt.subplot2grid((num_rows, 2), (0, 0))]
+    ax_sum = [plt.subplot2grid((num_rows, 2), (0, 1))]
+    for x in range(1, num_rows):
+        ax_iq.append(plt.subplot2grid((num_rows, 2), (x, 0)))
+        ax_sum.append(plt.subplot2grid((num_rows, 2), (x, 1)))
         ax_iq[-1].sharey(ax_iq[0])
         ax_sum[-1].sharey(ax_sum[0])
 else:
     # set up IQ axis for grid plotting
-    grid_sz = 2**(n//2)
-    ax_iq = [plt.subplot2grid((grid_sz, grid_sz), (0, 0))]
+    num_rows = 2**(n // 2)
+    num_cols = 2**(n - (n // 2))
+    ax_iq = [plt.subplot2grid((num_rows, num_cols), (0, 0))]
     ax_sum = None
-    for x in range(grid_sz):
-        for y in range(grid_sz):
+    for x in range(num_cols):
+        for y in range(num_rows):
             if x == 0 and y == 0:
                 continue
-            ax_iq.append(plt.subplot2grid((grid_sz, grid_sz), (y, x)))
+            ax_iq.append(plt.subplot2grid((num_rows, num_cols), (y, x)))
             ax_iq[-1].sharey(ax_iq[0])
 
 
 # plot all I and Q waves
 if ax_sum is not None:
-    for ax1, ax2, i, q in zip(ax_iq, ax_sum, i_waves, q_waves):
+    for ax1, ax2, (i, q) in zip(ax_iq, ax_sum, iq_waves):
         ax1.plot(time, i)
         ax1.plot(time, q)
         ax2.plot(time, i+q)
 else:
-    for ax1, i, q in zip(ax_iq, i_waves, q_waves):
+    for ax1, (i, q) in zip(ax_iq, iq_waves):
         ax1.plot(time, i)
         ax1.plot(time, q)
 
@@ -80,7 +86,7 @@ plt.figure()
 ax_const = plt.subplot()
 
 # plot constellation diagram
-ax_const.scatter(i_symbols, q_symbols)
+ax_const.scatter([iq[0] for iq in iq_symbols], [iq[1] for iq in iq_symbols])
 ax_const.axhline(y=0, color='k', linewidth=.5)
 ax_const.axvline(x=0, color='k', linewidth=.5)
 ax_const.set_aspect('equal', 'box')
