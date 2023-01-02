b2014bf45eb15bb855943d0f35059991656e05d0
J-K-M <90958043+J-K-M@users.noreply.github.com>
2023-01-02 22:28:37 +0000

    remove rows plotting mode

--- qam.py
+++ qam.py
@@ -6,9 +6,6 @@ time = np.arange(0, 40 * np.pi) / 10
 
 # Number of bits per QAM symbol
 n = 4
-# 'grid' shows IQ waveforms in grid, 'rows' shows IQ | Sum in rows
-mode = 'rows'
-
 
 def map_iq(i_bits: list[int], q_bits: list[int]) -> complex:
     ''' takes in-phase and quadrature bits to generate a complex IQ symbol '''
@@ -40,56 +37,33 @@ for iq in iq_symbols:
         iq.real * np.cos(time) + iq.imag * np.sin(time) * 1j
     )
 
-if mode == 'rows':
-    # set up IQ and Sum plot axis for side-by-side plotting
-    num_rows = 2**n
-    ax_iq: list[plt.Axes] = [plt.subplot2grid((num_rows, 2), (0, 0))]
-    ax_sum: list[plt.Axes] = [plt.subplot2grid((num_rows, 2), (0, 1))]
-    for x in range(1, num_rows):
-        ax_iq.append(plt.subplot2grid((num_rows, 2), (x, 0)))
-        ax_sum.append(plt.subplot2grid((num_rows, 2), (x, 1)))
-        ax_iq[-1].sharey(ax_iq[0])
-        ax_sum[-1].sharey(ax_sum[0])
-elif mode == 'grid':
-    # set up IQ axis for grid plotting
-    num_rows = 2**(n // 2)
-    num_cols = 2**(n - (n // 2))
-    ax_iq: list[plt.Axes] = [plt.subplot2grid((num_rows, num_cols), (0, 0))]
-    ax_sum = None
-    for x in range(num_cols):
-        for y in range(num_rows):
-            if x == 0 and y == 0:
-                continue
-            ax_iq.append(plt.subplot2grid((num_rows, num_cols), (y, x)))
-            ax_iq[-1].sharey(ax_iq[0])
-else:
-    raise ValueError("Error, invalid value for mode. Must be either 'rows' or 'grid'")
+
+# set up IQ axis for grid plotting
+num_rows = 2**(n // 2)
+num_cols = 2**(n - (n // 2))
+axis: list[plt.Axes] = [plt.subplot2grid((num_rows, num_cols), (0, 0))]
+for x in range(num_cols):
+    for y in range(num_rows):
+        if x == 0 and y == 0:
+            continue
+        axis.append(plt.subplot2grid((num_rows, num_cols), (y, x)))
+        axis[-1].sharey(axis[0])
 
 
 # plot all I and Q waves
-if mode == 'rows':
-    for ax1, ax2, iq in zip(ax_iq, ax_sum, iq_waves):
-        ax1.plot(time, iq.real)
-        ax1.plot(time, iq.imag)
-        ax2.plot(time, iq.real+iq.imag)
-elif mode == 'grid':
-    for ax1, iq in zip(ax_iq, iq_waves):
-        ax1.plot(time, iq.real)
-        ax1.plot(time, iq.imag)
-else:
-    raise ValueError("Error, invalid value for mode. Must be either 'rows' or 'grid'")
+for ax, iq in zip(axis, iq_waves):
+    ax.plot(time, iq.real, '--')
+    ax.plot(time, iq.imag, '--')
+    ax.plot(time, iq.real+iq.imag)
 
-
-plt.figure()
-# setup constellation diagram axis
-ax_const = plt.subplot()
 
 # plot constellation diagram
+plt.figure()
+ax_const = plt.subplot()
 ax_const.scatter([iq.real for iq in iq_symbols], [iq.imag for iq in iq_symbols])
 ax_const.axhline(y=0, color='k', linewidth=.5)
 ax_const.axvline(x=0, color='k', linewidth=.5)
 ax_const.set_aspect('equal', 'box')
-
 ax_const.set_title(f"QAM - {n} bits per symbol")
 
 
